1ab8ee158ee8ffe15ad2dfab6491310de7aad030
athulkdinu <athulkdinu@gmail.com>
2025-10-16 11:34:53 +0530

    updates

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // import the json server
 
-const JSONServer = require('json-server')
+const JSONServer = require('json-server')  // it return a express server
 
 // create server for running json file
 
-const rBuilder = JSONServer.create()
+const rBuilder = JSONServer.create()      
 
 // 3. create middleware
 
-const middleware=JSONServer.defaults()
+const middleware=JSONServer.defaults() 
 
 //6.import db,json file
 
-const router=JSONServer.router("db.json")
+const router=JSONServer.router("db.json") 
 
 //4.defime port to run the server
 
 const PORT=3000 || process.env.PORT
 //5.use middleware
 
-rBuilder.use(middleware)
+rBuilder.use(middleware) 
 //7.use router
 
 rBuilder.use(router)
@@ -30,3 +30,5 @@ rBuilder.listen(PORT,()=>{
     console.log(`resume builder starts at PORT number ${PORT}`);
     
 })
+
+
